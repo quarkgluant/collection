be2b6a1596842911f7de8b7e96fbedfe558caf42
Patrice Cadiot <pathibul.r@gmail.com>
2016-04-21 12:49:44 +0200

    3 challenges sauf level 3 du premier challenge

--- collection.rb
+++ collection.rb
@@ -61,3 +61,44 @@ end
 capital_name = "Denver"
 states.invert[capitals.invert[capital_name]]
 
+### 3: Scrabble Scoring
+
+# Start with this hash:
+
+# ```
+scores = {
+  "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+  "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+  "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+  "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+  "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+  "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+  "Y"=>4, "Z"=>10
+}
+
+### Level 1: Letter Scoring an Array
+# Write code to output the score for the letters in an array like this:
+# word = ['H', 'E', 'L', 'L', 'O']
+# Which, when run, outputs this:
+# H has the value 4
+# E has the value 1
+# L has the value 1
+# L has the value 1
+# O has the value 1
+# ```
+word.each{|lettre| puts "#{lettre} has the value #{scores[lettre]}" }
+
+### Level 2: Scoring Letters in a String
+# Write code to output the score for the letters in a string like this:
+word.each_char{|lettre| puts "#{lettre} has the value #{scores[lettre.upcase]}" }
+
+#### Level 3: Scoring an Entire String
+# Write code to output the total score for a string like this:
+# word = "hello"
+# Which, when run, outputs this:
+# hello has the total score 8
+acc = 0
+word.each_char do |lettre|
+	acc += scores[lettre.upcase]
+end
+puts "#{word} has the total score #{acc}" 
